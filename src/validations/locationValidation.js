f011574fec6492a83b078cf6500184f1172395b1
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-06-05 16:17:16 +0300

    Add location validation schemas, probably need to bind it with with region and type schemas

---
+++ src/validations/locationValidation.js
@@ -0,0 +1,43 @@
+import { Joi, Segments } from "celebrate";
+import { isValidObjectId } from "mongoose";
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message("Invalid id format") : value;
+};
+
+export const getAllLocationsSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    limit: Joi.number().integer().min(1).max(100).default(10),
+    region: Joi.string().max(64),
+    type: Joi.string().max(64),
+    search: Joi.string().allow("").max(96),
+  }),
+};
+
+export const locationIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    locationId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
+
+export const createLocationSchema = {
+  [Segments.BODY]: Joi.object({
+    name: Joi.string().min(3).max(96).required(),
+    type: Joi.string().max(64).required(),
+    region: Joi.string().max(64).required(),
+    description: Joi.string().min(20).max(6000).required(),
+  }),
+};
+
+export const updateLocationSchema = {
+  [Segments.PARAMS]: Joi.object({
+    locationId: Joi.string().custom(objectIdValidator).required(),
+  }),
+  [Segments.BODY]: Joi.object({
+    name: Joi.string().min(3).max(96).required(),
+    type: Joi.string().max(64).required(),
+    region: Joi.string().max(64).required(),
+    description: Joi.string().min(20).max(6000).required(),
+  }),
+};
