959b3ba98e23ff364f6b03426bab26b968326e95
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-06-05 17:48:04 +0300

    Add routes for locations requests

--- src/routes/locationsRouter.js
+++ src/routes/locationsRouter.js
@@ -1,5 +1,40 @@
 import { Router } from "express";
+import { celebrate } from "celebrate";
+import { locations } from "../controllers/index";
+import { authenticate } from "../middleware/authenticate.js";
+import {
+  createLocationSchema,
+  getAllLocationsSchema,
+  locationIdSchema,
+  updateLocationSchema,
+} from "../validations/locationsValidation.js";
 
 const locationsRouter = Router();
+
+router.get(
+  "/api/locations",
+  celebrate(getAllLocationsSchema),
+  locations.getAllLocations,
+);
+
+router.get(
+  "/api/locations/:locationId",
+  celebrate(locationIdSchema),
+  locations.getLocationById,
+);
+
+router.post(
+  "/api/locations",
+  authenticate,
+  uploadLocationImage.single("images"),
+  celebrate(createLocationSchema),
+  locations.createLocation,
+);
+router.patch(
+  "/api/locations/:locationId",
+  authenticate,
+  celebrate(updateLocationSchema),
+  locations.updateLocation,
+);
 
 export default locationsRouter;
